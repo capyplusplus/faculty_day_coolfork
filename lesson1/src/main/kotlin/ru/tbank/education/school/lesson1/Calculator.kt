3d26b29b1663a58403dd4b58b4c6aa2409836c67
Maltsev Timur <methyapple@gmail.com>
2025-11-13 10:57:20 +0300

    test commit

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -1,29 +1,70 @@
 package ru.tbank.education.school.lesson1
 import kotlin.math.*
-/**
- * Метод для вычисления простых арифметических операций.
- */
-fun calculate(a: Double, b: Double, operation: OperationType = OperationType.ADD): Double? {
-    val result = when (operation) {
-        OperationType.ADD -> a + b
-        OperationType.SUBTRACT -> a - b
-        OperationType.MULTIPLY -> a * b
-        OperationType.DIVIDE -> {
-            if (b == 0.0) {
-                null
-            } else {
-                a / b
-            }
-        }
+import java.math.BigDecimal
+import java.math.RoundingMode
 
-        OperationType.SIN -> sin(a)
-        OperationType.COS -> cos(a)
-        OperationType.TG -> tan(a)
-        OperationType.CTG -> 1 / tan(a)
-        else -> null
+/**
+ * Функция преобразует математическое выражение в постфиксную запись (обратная польская нотация)
+ * @sample express математическое выражение, которое надо преобразовать
+ * @return Это же выражение в постфиксной записи
+ */
+fun expressionToPostfix(express:String): String {
+
+    return ""
+}
+
+
+fun BigDecimal.floor(): BigDecimal {
+    return this.setScale(0, RoundingMode.FLOOR)
+}
+
+fun sin(x: BigDecimal): BigDecimal {
+    val y = (x / (2 * PI).toBigDecimal()).floor()
+    val dx = x - ((2 * PI).toBigDecimal() * y);
+    return sin(dx)
+}
+
+/**
+ * Функция убирает последний элемент из стэка и возвращает его
+ * @return удалённый элемент стэка
+ */
+fun MutableList<BigDecimal>.pop(): BigDecimal {
+    val a = this[this.size - 1]
+    this.pop()
+    return a
+}
+
+fun MutableList<BigDecimal>.applyOp(block: (BigDecimal, BigDecimal) -> BigDecimal) {
+    val a = this.pop()
+    val b = this.pop()
+    add(block(b, a))
+}
+
+fun MutableList<BigDecimal>.applyOp(block: (BigDecimal) -> BigDecimal) {
+    val a = this.pop()
+    add(block(a))
+}
+
+/**
+ * Вычисляет выражение, записанное в постфиксной записи
+ * @sample express математическое выражение, которое надо вычислить
+ * @return Результат вычислений
+ */
+fun calculatePostfixExpression(express: String):String? {
+    var itemStack: MutableList<BigDecimal> = mutableListOf()
+    for (item in express.split(" ")) {
+        when (item) {
+            "+" -> itemStack.applyOp { x, y -> x + y }
+            "-" -> itemStack.applyOp { x, y -> x - y }
+            "*" -> itemStack.applyOp { x, y -> x * y }
+            "/" -> itemStack.applyOp { x, y -> x / y }
+            "%" -> itemStack.applyOp { x, y -> x % y }
+            "sin" -> itemStack.applyOp { x -> sin(x) }
+            else -> itemStack.add(item.toBigDecimalOrNull() ?: return null)
+        }
     }
 
-    return result
+    return null
 }
 
 /**
@@ -33,53 +74,6 @@ fun calculate(a: Double, b: Double, operation: OperationType = OperationType.ADD
  */
 @Suppress("ReturnCount")
 fun String.calculate(): Double? {
-    var arr = this.split(" ")
-    var operator: OperationType? = null
-    var a: Double? = null
-    var b: Double? = null
 
-    if (arr.size == 3) {
-        if (arr[1] == "+") {
-            operator = OperationType.ADD
-        } else if (arr[1] == "-") {
-            operator = OperationType.SUBTRACT
-        } else if (arr[1] == "*") {
-            operator = OperationType.MULTIPLY
-        } else if (arr[1] == "/") {
-            operator = OperationType.DIVIDE
-        } else {
-            return null
-        }
-        a = arr[0].toDoubleOrNull()
-        b = arr[2].toDoubleOrNull()
-    } else if (arr.size == 2) {
-        arr[0].lowercase()
-        if (arr[0] == "sin") {
-            operator = OperationType.SIN
-        } else if (arr[0] == "cos") {
-            operator = OperationType.COS
-        } else if (arr[0] == "tg" || arr[0] == "tan") {
-            operator = OperationType.TG
-        } else if (arr[0] == "ctg" || arr[0] == "ctan") {
-            operator = OperationType.CTG
-        } else {
-            return null
-        }
-        val inDegrees: Boolean = (arr[1].get(arr[1].length - 1) == '*')
-        val value = if(!inDegrees) arr[1] else arr[1].dropLast(1)
-
-        a = value.toDoubleOrNull()
-        if (inDegrees && a != null) {
-            a = a / 180 * 3.14
-        }
-        b = 0.0
-    } else {
-        return null
-    }
-
-    if (a == null || b == null || operator == null) {
-        return null
-    }
-
-    return calculate(a = a, b = b, operator)
+    return 0.0
 }
