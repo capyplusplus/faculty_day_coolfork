237b402a963910b2f34f912cbadcf63568b3e3c5
Maltsev Timur <methyapple@gmail.com>
2025-11-13 10:57:55 +0300

    test commit

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -1,6 +1,40 @@
 package ru.tbank.education.school.lesson1
 
+open class Food {
+    protected var condition:Int = 100
+    open fun eat (bite:Int = 10) {
+        condition -= bite
+    }
+}
+class Apple (
+    val color:String,
+    val shape:String,
+    val taste:String,
+    val poisoned: Boolean,
+    var hasLeaf: Boolean = false
+    ): Food() {
 
+    override fun eat(bite:Int) {
+        if (!poisoned) {
+            super.eat(bite)
+        } else {
+            error("the apple is poisoned!")
+        }
+    }
+
+    fun getCondition():Int {
+        return condition
+    }
+}
+
+/**
+ * МОДИФИКАТОРЫ ДОСТУПА:
+ * private - недоступно извне
+ * protected - недоступно извне, но доступно для дочерних классов
+ * internal - доступ только внутри пакета
+ * public - полная доступность (по дефолту)
+ */
+// Data классы запрещают наследование
 fun main() {
     val str:String = "const string"
     println(str)
@@ -12,4 +46,10 @@ fun main() {
     val n:Byte? = null
     println(n)
 
+    for (x in 5 downTo 1 step 2) {
+        println(x)
+    }
+
+    val apple = Apple("Green", "Cube", "Juicy", false)
+    println("Цвет яблока: ${apple.color}")
 }
